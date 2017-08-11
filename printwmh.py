c0144d1ef1d9763edbadf519e467a746fca70fbb
daerlnaxe <axe.test1@gmail.com>
2017-08-11 18:49:30 +0200

    Add files via upload

--- printwmh.py
+++ printwmh.py
@@ -9,18 +9,21 @@
     créé le: 2017-08-10
     modifié le: 2017-08-10
 
+    A function print with 2 modes, a normal mode to act like the old and a mode to work with a message handler, to inject code
+    directly in the message, to struct it differently in order to manage it more easyly when intercepted after.
+
+    Can Work with another script i did: intercepteur.py if you don't want to develop your own
+
     Sorry for my english, i'm french
 
-    juste a word: Seriously i don't know at all if something like this exists, i'm a great noob. Just 1 month with it.
-    I dit that for me, i said to my self, perhaps it can be usefull for others, so....
-
+    Just a word: Seriously i don't know at all if something like this exists, i'm a great noob.
+    Just 1 month with Python. I made it that for me, i said to myself, perhaps it can be useful for others, so....
 
     This function can act as the normal printing and permit to interface in the other mode with a message handler
     For the second part: it write at the end the message for the part councerning the interfacing with a handler because i think '\n' parasite them.)
 
-    mHandler: message handler
     To interface with a message handler, you must intercept 'write' like this: sys.stdout = 'yourclass'
-    (to return to the normal writing )
+    (to return to the normal writing: sys.stdout = sys.__stdout__ )
     In this mode the message is prefixed with a name of handler and a level of message (so you can redirect flux as
     by the handler, than by the level message or both.
 
@@ -44,8 +47,10 @@
         - file: to redirect the output to a file
 
     mode with handler:
-
-
+        - mHandler: handler's name
+        - lvlMess: level of your message (you can use what you want, numbers, string...)
+        - sepInfo: The separator string you want to use to adapt this to your needs
+        - start: No common thing with a handler message, but i use it to begin a message by what i want. Sometimes a '\n'
 
 """
 
